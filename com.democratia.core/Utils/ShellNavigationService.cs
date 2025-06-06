f0328d6f8adf43788bfb99a6097d95a5213549dd
zykogithub <modaherry@gmail.com>
2025-06-06 12:03:49 +0200

    correction mineur

--- com.democratia.core/Utils/ShellNavigationService.cs
+++ com.democratia.core/Utils/ShellNavigationService.cs
@@ -5,7 +5,7 @@ public class ShellNavigationService : INavigationService
     public Task GoToAsync(string route, ShellNavigationQueryParameters? parameters = null)
     {
         // Ensure parameters is not null by initializing it if necessary
-        parameters ??= new ShellNavigationQueryParameters();
+        parameters ??= [];
 
         // Example implementation of navigation logic
         return Shell.Current.GoToAsync(route, parameters);
