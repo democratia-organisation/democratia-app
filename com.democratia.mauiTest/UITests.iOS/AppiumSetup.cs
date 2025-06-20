a114fbd5aac2ada764e235b9565d399aaabd910f
zykogithub <modaherry@gmail.com>
2025-06-20 09:38:16 +0200

    modification mineur pour préparer le test ui sur iOS et macOS

--- com.democratia.mauiTest/UITests.iOS/AppiumSetup.cs
+++ com.democratia.mauiTest/UITests.iOS/AppiumSetup.cs
@@ -20,9 +20,9 @@ namespace UITests
                 // iOS Version
                 PlatformVersion = "17.0",
                 // Don't specify if you don't want a specific device
-                DeviceName = "iPhone 15 Pro",
+                DeviceName = "device",
                 // The full path to the .app file to test or the bundle id if the app is already installed on the device
-                App = "com.democratia",
+                App = "C:\\Users\\naher\\Documents\\autre\\projet\\projets_personnel\\democratia\\application\\com.democratia.view\\bin\\Debug\\net9.0-ios\\iossimulator-x64\\com.democratia.view.app",
             };
 
             // Note there are many more options that you can use to influence the app under test according to your needs
