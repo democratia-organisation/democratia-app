c453fa9f6e4db8de6c7e0b0764fdfa3ab9cae1c4
zykogithub <modaherry@gmail.com>
2025-06-23 20:24:49 +0200

    début test UI et test unitairte de la page Creation

---
+++ com.democratia.mauiTest/UITests.Shared/View/CreactionPageTest.cs
@@ -0,0 +1,34 @@
+﻿using Xunit;
+
+namespace UITests.View
+{
+    public class CreationPageTest : BaseTest, IClassFixture<CreationPageTestFixture>
+    {
+        private readonly MainPageTestFixture? _mainPageTestFixture;
+
+        public CreationPageTest(MainPageTestFixture? mainPageTestFixture)
+        {
+            if (SystemInfo.SSHHost()) return;
+            if (AppiumSetup.device == "windows") AppiumSetup.RunBeforeAnyTests();
+            _mainPageTestFixture = mainPageTestFixture;
+
+        }
+
+        public override void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class CreationPageTestFixture : IDisposable
+    {
+        public CreationPageTestFixture()
+        {
+            // Initialization code for the fixture
+        }
+        public void Dispose()
+        {
+            // Cleanup code for the fixture
+        }
+    }   
+}
