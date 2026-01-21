a4d8e88990be47a28b3a3abdae20eaf027eca6b9
Zyko <modaherry@gmail.com>
2026-01-21 11:47:54 +0100

    bfbg

--- com.democratia.view/Views/Home.cs
+++ com.democratia.view/Views/Home.cs
@@ -6,31 +6,33 @@ namespace com.democratia.view.Views
 {
     public partial class Home : ContentPage
     {
+        private readonly VerticalStackLayout _stackLayout;
+        private readonly CollectionView _collectionView;
+        private readonly Button _createGroupButton;
+        private readonly Label _ownGroupeLabel;
+
         public Home(HomeViewModel viewModel)
         {
-            
             BindingContext = viewModel;
+
             if (Application.Current!.Resources.TryGetValue("Light-background", out var light) && Application.Current!.Resources.TryGetValue("Dark-background", out var dark))
             {
-                Color lightCouleur = (Color)light, darkCouleur = (Color)dark;
+                Color lightCouleur = (Color)light;
+                Color darkCouleur = (Color)dark;
                 this.SetAppThemeColor(BackgroundColorProperty, lightCouleur, darkCouleur);
             }
 
-            Content = new VerticalStackLayout
+            this._stackLayout = new VerticalStackLayout
             {
                 Children =
                 {
                     new Header(),
                     new Grid
                     {
-                        RowDefinitions =
-                        {
-                            new RowDefinition { Height = GridLength.Star }, // Prend tout l'espace
-                            new RowDefinition { Height = GridLength.Auto }  // Prend juste la place du bouton
-                        },
+                        HeightRequest = 80,
+                        WidthRequest = 200,
                         Children =
                         {
-
                             new BoxView { WidthRequest = 700 },
                             new ImageButton
                             {
@@ -39,81 +41,93 @@ namespace com.democratia.view.Views
                                 HeightRequest = 50,
                                 WidthRequest = 50,
                             }
-                        },
-                        HeightRequest = 80,
-                        WidthRequest = 200,
-                    },
-                    new BoxView { HeightRequest = 50 },
-                    new Label
-                    {
-                        Text = "Mes Groupes",
-                        Style = (Style?)Application.Current?.Resources["HeadlineStyle"],
-                        HorizontalOptions = LayoutOptions.Center
-                    },
-                    new BoxView { HeightRequest = 50 },
-                    new CollectionView
-                    {
-                        ItemsSource = ((HomeViewModel)BindingContext).groupes,
-                        ItemsLayout = new GridItemsLayout(3, ItemsLayoutOrientation.Vertical)
-                        {
-                            HorizontalItemSpacing = 10,
-                            VerticalItemSpacing = 10
-                        },
-                        ItemTemplate = new DataTemplate(() =>
-                        {
-
-                            var container =  new Border()
-                            {
-                                Style = (Style?)Application.Current?.Resources["BorderStyle"],
-                            };
-                            container.BindingContextChanged += (s, e) =>
-                            {
-                                var border = (Border)s;
-                                if (border?.BindingContext is Models.Groupe groupe && BindingContext is HomeViewModel viewModel)
-                                {
-                                    var buttonGroupe = new ButtonGroupe(groupe.Image, groupe.NomGroupe, viewModel.OpenGroupCommand, groupe.IdGroupe, viewModel);
-                                    border.Content = buttonGroupe;
-                                }
-                            };
-                            return container;
-
-                        }),
-                    },
-                    new BoxView { HeightRequest = 80 },
-                    new Button
-                    {
-                        Text = "Créer un nouveau groupe",
-                        Style = (Style?)Application.Current?.Resources["ButtonStyle"],
-                        Command = viewModel.NavigateTappedCommand,
-                        CommandParameter = "CreerGroupe",
-                        VerticalOptions = LayoutOptions.End
+                        }
                     }
                 }
             };
 
-        }
-
-        private Grid? NewGrid()
-        {
-            if (BindingContext is HomeViewModel viewModel)
+            this._ownGroupeLabel = new Label
             {
-                var grille = new Grid() { Padding = 10 };
-                grille.ColumnDefinitions.Add(new ColumnDefinition { Width = GridLength.Star });
-                grille.ColumnDefinitions.Add(new ColumnDefinition { Width = GridLength.Star });
-                grille.ColumnDefinitions.Add(new ColumnDefinition { Width = GridLength.Star });
-                for (int i = 0; i < viewModel.groupes.Count; i++)
-                    grille.Add(new ButtonGroupe(viewModel.groupes[i].Image, viewModel.groupes[i].NomGroupe, viewModel.OpenGroupCommand, viewModel.groupes[i].IdGroupe, (HomeViewModel)BindingContext));
-                return grille;
-            }
-            else return null;
-            
+                Text = "Mes Groupes",
+                Style = (Style?)Application.Current?.Resources["HeadlineStyle"],
+                HorizontalOptions = LayoutOptions.Center,
+                Margin = new Thickness(0, 20)
+            };
+
+            this._collectionView = new CollectionView
+            {
+                ItemsSource = viewModel.groupes,
+                VerticalOptions = LayoutOptions.Fill,
+                ItemsLayout = new GridItemsLayout(3, ItemsLayoutOrientation.Vertical)
+                {
+                    HorizontalItemSpacing = 10,
+                    VerticalItemSpacing = 10
+                },
+                ItemTemplate = new DataTemplate(() =>
+                {
+                    var container = new Border
+                    {
+                        Style = (Style?)Application.Current?.Resources["BorderStyle"],
+                    };
+
+                    container.BindingContextChanged += (s, e) =>
+                    {
+                        var border = (Border?)s;
+                        if (border?.BindingContext is Models.Groupe groupe && this.BindingContext is HomeViewModel vm)
+                        {
+                            border.Content = new ButtonGroupe(
+                                groupe.Image,
+                                groupe.NomGroupe,
+                                vm.OpenGroupCommand,
+                                groupe.IdGroupe,
+                                vm
+                            );
+                        }
+                    };
+                    return container;
+                }),
+            };
+
+            this._createGroupButton = new Button
+            {
+                Text = "Créer un nouveau groupe",
+                Style = (Style?)Application.Current?.Resources["ButtonStyle"],
+                Command = viewModel.NavigateTappedCommand,
+                CommandParameter = "CreerGroupe",
+                VerticalOptions = LayoutOptions.End,
+                Margin = new Thickness(20)
+            };
+
+            var grillePrincipale = new Grid
+            {
+                RowDefinitions =
+                {
+                    new RowDefinition { Height = GridLength.Auto },
+                    new RowDefinition { Height = GridLength.Auto },
+                    new RowDefinition { Height = GridLength.Star },
+                    new RowDefinition { Height = GridLength.Auto }                  
+                 },
+                Children =
+                {
+                    this._stackLayout,
+                    this._ownGroupeLabel,
+                    this._collectionView,
+                    this._createGroupButton
+                }
+            };
+
+            Grid.SetRow(this._stackLayout, 0);
+            Grid.SetRow(this._ownGroupeLabel, 1);
+            Grid.SetRow(this._collectionView, 2);
+            Grid.SetRow(this._createGroupButton, 3);
+
+            Content = grillePrincipale;
         }
 
         protected async override void OnAppearing()
         {
             base.OnAppearing();
 
-            // 1. Récupérer le ViewModel depuis le BindingContext
             if (BindingContext is HomeViewModel vm)
             {
                 vm.InitializeAsync();
