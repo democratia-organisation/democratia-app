d57d0f7d2b29e7c95d7678b4b89bdca99a66996f
Zyko <modaherry@gmail.com>
2026-01-25 02:21:42 +0100

    changement de style, internationalisation et préparation pour changement dynamique de langue

--- com.democratia.view/Views/Component/Header.xaml.cs
+++ com.democratia.view/Views/Component/Header.xaml.cs
@@ -1,3 +1,9 @@
+using com.democratia.view.Resources.Localization;
+using CommunityToolkit.Mvvm.Messaging;
+using CommunityToolkit.Mvvm.Messaging.Messages;
+using System.Globalization;
+using com.democratia.ViewModels;
+
 namespace com.democratia.Views.Component;
 
 public partial class Header : ContentView
@@ -5,39 +11,44 @@ public partial class Header : ContentView
     public Header()
     {
         InitializeComponent();
-        SetTheme();
+        SetTheme()  ;
     }
 
 
     private async void OnClicked(object sender, EventArgs e)
     {
-        ImageButton button = (ImageButton)sender;
-        if (button == switchImageButton)
+        if (sender is ImageButton button)
         {
-            if (Application.Current?.RequestedTheme == AppTheme.Dark)
+            if (button == switchImageButton)
             {
-                Application.Current.UserAppTheme = AppTheme.Light;
-                button.Source = "light.png";
+                if (Application.Current?.RequestedTheme == AppTheme.Dark)
+                {
+                    Application.Current.UserAppTheme = AppTheme.Light;
+                    button.Source = "light.png";
+                }
+                else if (Application.Current?.RequestedTheme == AppTheme.Light)
+                {
+                    Application.Current.UserAppTheme = AppTheme.Dark;
+                    button.Source = "dark.png";
+                }
             }
-            else if (Application.Current?.RequestedTheme == AppTheme.Light)
+            else if (button == backButton)
             {
-                Application.Current.UserAppTheme = AppTheme.Dark;
-                button.Source = "dark.png";
+                await AppShell.Current.GoToAsync("..");
             }
+            else
+            {
+                await AppShell.Current.GoToAsync("//Home"); // TODO l'autoriser à y aller que s'il est connecté
+            }
+
         }
-        else if (button == backButton)
+        else if (sender is Picker picker)
         {
-            await AppShell.Current.GoToAsync("..");
-        }
-        else
-        {
-            await AppShell.Current.GoToAsync("//MainPage");
+            // changer la langue
+
         }
     }
 
     private void SetTheme() =>
         switchImageButton.Source = Application.Current?.RequestedTheme == AppTheme.Dark ? "dark.png" : "light.png";
-
-
-
 }
