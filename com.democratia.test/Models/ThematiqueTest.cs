aed3ef9e163acc674c694469fa7647d565cdfa32
Zyko <modaherry@gmail.com>
2026-02-17 16:08:19 +0100

    début écriture des tests

---
+++ com.democratia.test/Models/ThematiqueTest.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace com.democratia.test.Models
+{
+    internal class ThematiqueTest
+    {
+    }
+}
---
+++ com.democratia.test/ViewModels/internaute/gestionCompte/PreferenceViewModelTest.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace com.democratia.test.ViewModels.internaute.gestionCompte
+{
+    internal class PreferenceViewModelTest
+    {
+    }
+}
