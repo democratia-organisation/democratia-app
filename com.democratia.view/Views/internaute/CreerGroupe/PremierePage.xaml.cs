0bf7ff58980cd608ddb9852c57ef9c87996a1346
Zyko <modaherry@gmail.com>
2026-02-18 11:40:12 +0100

    refacto pour visual paradigm suite

--- com.democratia.view/Views/internaute/CreerGroupe/PremierePage.xaml.cs
+++ com.democratia.view/Views/internaute/CreerGroupe/PremierePage.xaml.cs
@@ -1,13 +1,14 @@
 using com.democratia.ViewModels.internaute.CreerGroupe;
 
-namespace com.democratia.Views.internaute.CreerGroupe;
-
-public partial class PremierePage : ContentPage
+namespace com.democratia.Views.internaute.CreerGroupe
 {
-	public PremierePage(PremierePageViewModel viewModel)
-	{
-		BindingContext = viewModel;
-        InitializeComponent();
-        behavior.BindingContext = BindingContext;
+    public partial class PremierePage : ContentPage
+    {
+        public PremierePage(PremierePageViewModel viewModel)
+        {
+            BindingContext = viewModel;
+            InitializeComponent();
+            behavior.BindingContext = BindingContext;
+        }
     }
 }
