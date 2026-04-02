19b7c2d03cc26c230723e7efbb62a38924c3048f
Zyko <modaherry@gmail.com>
2026-04-02 23:50:46 +0200

    feat: implémentation d'une logique d'autoconnexion

--- com.democratia.view/App.xaml.cs
+++ com.democratia.view/App.xaml.cs
@@ -3,6 +3,9 @@ using com.democratia.Platforms.Android;
 using AndroidX.Core.View;
 using Microsoft.Maui.Handlers;
 #endif
+using com.democratia.ViewModels.internaute;
+using com.democratia.Views;
+using com.democratia.Views.internaute;
 
 namespace com.democratia
 {
@@ -12,8 +15,7 @@ namespace com.democratia
         public App()
         {
             InitializeComponent();
-            Current?.UserAppTheme =
-                (AppTheme)Preferences.Default.Get("Theme", (int)Current?.UserAppTheme!)!;
+            Current?.UserAppTheme = (AppTheme)Preferences.Default.Get("Theme", (int)Current?.UserAppTheme!)!;
 
 #if ANDROID
             // Permet de gérer l'AutomationId pour les tests UI sur Android
@@ -32,17 +34,14 @@ namespace com.democratia
             {
                 AndroidHandler(handler, view, previousAction);
             });
-            MainApplication.SetLocal((string)Preferences.Default.Get("Language", MainApplication.cultureInfo.Name));
+            MainApplication.SetLocal(Preferences.Default.Get("Language", MainApplication.cultureInfo.Name));
 #elif WINDOWS
-            WinUI.App.SetLocal((string)Preferences.Default.Get("Language", WinUI.App.cultureInfo.Name));
+            WinUI.App.SetLocal(Preferences.Default.Get("Language", WinUI.App.cultureInfo.Name));
 #elif IOS || MACCATALYST
-            AppDelegate.SetLocal((string)Preferences.Default.Get("Language", AppDelegate.cultureInfo.Name));
+            AppDelegate.SetLocal(Preferences.Default.Get("Language", AppDelegate.cultureInfo.Name));
 #endif
 
-
-
         }
-
 
 #if ANDROID
 
@@ -65,5 +64,22 @@ namespace com.democratia
 #endif
         protected override Window CreateWindow(IActivationState? activationState) => new(new AppShell());
 
+        protected async override void OnStart()
+        {
+            base.OnStart();
+            var homeViewModel = ServiceHelper.GetService<HomeViewModel>()!;
+            var viewModel = ServiceHelper.GetService<MainViewModel>();
+            MainThread.BeginInvokeOnMainThread(async () =>
+            {
+                await viewModel!.AutoConnect(homeViewModel);
+                if (viewModel.autoConnectSuccess)
+                {
+                    homeViewModel.internaute = viewModel.modele;
+                    Windows[0].Page = new HomePage(homeViewModel);
+                }
+                else
+                    Windows[0].Page = new MainPage(viewModel);
+            });
+        }
     }
 }
